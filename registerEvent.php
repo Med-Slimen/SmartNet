5c450499ef55b79aa3897508aeb0d25a5beec21e
Med-Slimen <mohamedamineslimene01@gmail.com>
2025-03-18 03:21:39 -1100

    Update registerEvent.php

--- registerEvent.php
+++ registerEvent.php
@@ -38,7 +38,7 @@ if (isset($_POST["register_fname"])) {
                 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                 $mail->Username   = 'mohamedamineslimene01@gmail.com';                     //SMTP username
-                $mail->Password   = '';                               //SMTP password
+                $mail->Password   = 'pbdobbakaqfbseng';                               //SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
